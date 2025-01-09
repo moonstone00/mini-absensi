cdf3232c4790e9dad039bd6ed261c80c66e49774
Ghifani <ghifani03@gmail.com>
2025-01-09 22:48:49 +0700

    ✨ Create endpoint
    Create endpoint at users.js

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,12 @@ const cors = require('cors')
 const mysql = require('mysql2')
 const port = 3200
 
+const userEndpoint = require('./routes/users')
+
 const app = express()
 app.use(cors())
 app.use(express.json())
+
+app.use('/', userEndpoint)
 
 app.listen(port, () => console.log(`running server on port ${port}`))
